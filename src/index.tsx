10c48ad251be824c3d3fbe2741244dd6d0fedf88
Shisei Hanai <shanai@jp.ibm.com>
2022-03-18 09:22:14 +0900

    pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <BrowserRouter basename="/mui-react-sample000">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>,
